af9127017830bab20e85a93cd0ed0b6f61181ec2
Guillaume <guillaume.turpin45@gmail.com>
2024-04-30 15:05:06 +0200

    :sparkles: Add progressbar to Command

---
+++ app/Console/Commands/DispatchUserSpotifyPlaylistsFetchCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use App\Jobs\FetchUserSpotifyPlaylistsJob;
+
+class DispatchUserSpotifyPlaylistsFetchCommand extends Command
+{
+    protected const CHUNK_SIZE       = 10;
+    protected const DELAY_IN_MINUTES = 5;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:dispatch-user-spotify-playlists-fetch';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dispatch jobs to fetch spotify playlists for all users to avoid API Rate limits';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Start dispatching jobs to fetch spotify playlists');
+
+        $users        = User::WithSpotifyAccountLinked()->get();
+        $progress_bar = $this->output->createProgressBar($users->count());
+
+        $users->chunk(self::CHUNK_SIZE)
+            ->reduce( function( int $accumulated_delay, $chunk ) use ( $progress_bar ) {
+                $chunk->each( function( User $user ) use ( $progress_bar, &$accumulated_delay ) {
+                    FetchUserSpotifyPlaylistsJob::dispatch($user)
+                        ->delay( now()->addMinutes( $accumulated_delay ) );
+                    $progress_bar->advance();
+                });
+
+                return $accumulated_delay + self::DELAY_IN_MINUTES;
+            }, 0 );
+
+        $progress_bar->finish();
+        $this->info("\n All jobs dispatched");
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Console\Commands\DispatchUserSpotifyPlaylistsFetch;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\DispatchUserSpotifyPlaylistsFetchCommand;
 
-Schedule::command(DispatchUserSpotifyPlaylistsFetch::class)
+Schedule::command(DispatchUserSpotifyPlaylistsFetchCommand::class)
     ->dailyAt('03:00');
