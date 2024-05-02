2b4dc12fe0ee516f071b25a9a4b9a6ae66bb32cb
Guillaume <guillaume.turpin45@gmail.com>
2024-05-02 21:56:28 +0200

    :recycle: Refacto Playlist store method

--- app/DataObjects/Spotify/PlaylistDTO.php
+++ app/DataObjects/Spotify/PlaylistDTO.php
@@ -2,29 +2,29 @@
 
 namespace App\DataObjects\Spotify;
 
+use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Attributes\MapName;
 
 /**
  * The Spotify Playlist Representation
  */
 final class PlaylistDTO extends Data {
     public function __construct(
-        #[MapName('id')]
+        #[MapInputName('id')]
         public readonly string $spotify_id,
         public readonly string $name,
         public readonly ?string $description,
 
-        #[MapName('images.0.url')]
+        #[MapInputName('images.0.url')]
         public readonly ?string $thumbnail_url,
 
-        #[MapName('owner.display_name')]
+        #[MapInputName('owner.display_name')]
         public readonly string $owner,
 
-        #[MapName('public')]
+        #[MapInputName('public')]
         public readonly bool $is_public,
 
-        #[MapName('collaborative')]
+        #[MapInputName('collaborative')]
         public readonly bool $is_collaborative
     ) {}
 }
--- app/Services/Spotify/PlaylistService.php
+++ app/Services/Spotify/PlaylistService.php
@@ -8,14 +8,6 @@ use App\Models\User;
 class PlaylistService
 {
     /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Store the playlist in the database from a DTO
      *
      * @param PlaylistDTO $playlist_dto The playlist data transfer object
@@ -23,14 +15,6 @@ class PlaylistService
      */
     public function store( PlaylistDTO $playlist_dto, User $user ): void
     {
-        $user->playlists()->create( [
-            'spotify_id'       => $playlist_dto->spotify_id,
-            'name'             => $playlist_dto->name,
-            'description'      => $playlist_dto->description,
-            'thumbnail_url'    => $playlist_dto->thumbnail_url,
-            'owner'            => $playlist_dto->owner,
-            'is_public'        => $playlist_dto->is_public,
-            'is_collaborative' => $playlist_dto->is_collaborative
-        ] );
+        $user->playlists()->create( $playlist_dto->toArray() );
     }
 }
