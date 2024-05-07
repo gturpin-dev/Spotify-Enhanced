8cc2f1ee40e688056666d89558967e36d72c8140
Guillaume <guillaume.turpin45@gmail.com>
2024-05-07 17:57:52 +0200

    :bug: Fix frontend filters

--- resources/views/livewire/playlists-paginated-index.blade.php
+++ resources/views/livewire/playlists-paginated-index.blade.php
@@ -1,22 +1,27 @@
 <div class="min-h-screen">
 
-        <div class="flex gap-4 mb-4">
-            <input type="text" wire:model.live.debounce.250ms="search" placeholder="{{ __('Search for playlists...') }}"
-                   class="flex-1 px-4 py-2 border rounded shadow focus:outline-none focus:border-blue-300">
+    <div class="flex gap-4 mb-4">
+        <input type="text" wire:model.live.debounce.250ms="search" placeholder="{{ __('Search for playlists...') }}"
+                class="flex-1 px-4 py-2 border rounded shadow focus:outline-none focus:border-blue-300">
 
-            <div class="flex space-x-2">
-                <button wire:click="sortBy('name')"
-                        class="px-4 py-2 text-white bg-gray-600 rounded hover:bg-gray-700 focus:outline-none">
-                    {{ __('Nom') }} <span>{{ $sortDirection === 'ASC' ? '↑' : '↓' }}</span>
-                </button>
+        <div class="flex space-x-2">
+            <button wire:click="sortBy('name')"
+                    class="{{ $sortField === 'name' ? 'text-blue-500' : 'text-white' }} px-4 py-2 bg-gray-600 rounded hover:bg-gray-700 focus:outline-none">
+                {{ __('Nom') }}
+                @if ($sortField === 'name')
+                    <span>{{ $sortDirection === 'ASC' ? '↑' : '↓' }}</span>
+                @endif
+            </button>
 
-                <button wire:click="sortBy('music_count')"
-                        class="px-4 py-2 text-white bg-gray-600 rounded hover:bg-gray-700 focus:outline-none">
-                        {{ __('Musique') }} <span>{{ $sortDirection === 'ASC' ? '↑' : '↓' }}</span>
-                </button>
-            </div>
+            <button wire:click="sortBy('music_count')"
+                    class="{{ $sortField === 'music_count' ? 'text-blue-500' : 'text-white' }} px-4 py-2 bg-gray-600 rounded hover:bg-gray-700 focus:outline-none">
+                {{ __('Musique') }}
+                @if ($sortField === 'music_count')
+                    <span>{{ $sortDirection === 'ASC' ? '↑' : '↓' }}</span>
+                @endif
+            </button>
         </div>
-
+    </div>
 
 
     <div class="grid grid-cols-5 gap-4">
