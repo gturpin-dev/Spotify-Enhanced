7a52f203e89e54f7f05754382c7c34d812089709
Guillaume <guillaume.turpin45@gmail.com>
2024-08-06 13:18:40 +0200

    :sparkles: Installs Passport and make routes to consume its features

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,13 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory,
+        Notifiable,
+        HasApiTokens;
 
     protected $guarded = [];
 
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ use App\Http\Controllers\SpotifyAuthController;
 use App\Http\Integrations\Spotify\SpotifyConnector;
 use App\Http\Middleware\EnsureSpotifyAccountLinked;
 use App\Http\Integrations\Spotify\Requests\GetPlaylistTracksRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\UrlHelper;
+
+use function Pest\Laravel\post;
 
 Route::get('/', function () {
     return view('welcome');
@@ -45,3 +50,52 @@ Route::get('/auth/spotify', [SpotifyAuthController::class, 'redirectToProvider']
 Route::get('/auth/spotify/callback', [SpotifyAuthController::class, 'handleProviderCallback']);
 
 require __DIR__.'/auth.php';
+
+
+/**
+ * Tests the passport from client side application
+ */
+Route::get( '/consuming-passport/auth', function( Request $request ) {
+    $request->session()->put( 'api-state', $state = $request->fingerprint() );
+
+    $query_string = http_build_query( [
+        'client_id'     => env( 'PASSPORT_CONSUMING_CLIENT_ID' ),
+        'redirect_uri'  => env( 'PASSPORT_CONSUMING_REDIRECT_URI' ),
+        'response_type' => 'code',
+        'scope'         => '',
+        'state'         => $state,
+    ] );
+
+    return redirect( env( 'PASSPORT_CONSUMING_AUTHORIZATION_URL' ) . '?' . $query_string );
+} );
+
+Route::get( '/consuming-passport/auth/callback', function( Request $request ) {
+    $state = $request->session()->pull( 'api-state' );
+
+    throw_unless(
+        ! empty( $state ) && $state === $request->input( 'state' ),
+        InvalidArgumentException::class,
+        'Invalid state value.'
+    );
+
+    $response = Http::asForm()->post( env( 'PASSPORT_CONSUMING_TOKEN_URL' ), [
+        'grant_type'    => 'authorization_code',
+        'client_id'     => env( 'PASSPORT_CONSUMING_CLIENT_ID' ),
+        'client_secret' => env( 'PASSPORT_CONSUMING_CLIENT_SECRET' ),
+        'redirect_uri'  => env( 'PASSPORT_CONSUMING_REDIRECT_URI' ),
+        'code'          => $request->input( 'code' ),
+    ] );
+
+    /**
+     * If need to refresh tokens
+     */
+    // $response = Http::asForm()->post('http://passport-app.test/oauth/token', [
+    //     'grant_type' => 'refresh_token',
+    //     'refresh_token' => 'the-refresh-token',
+    //     'client_id' => 'client-id',
+    //     'client_secret' => 'client-secret',
+    //     'scope' => '',
+    // ]);
+
+    return $response->json();
+} );
