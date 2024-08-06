fac6b83ae477d338aaa2bfd82573186ee726c95c
Guillaume <guillaume.turpin45@gmail.com>
2024-08-06 19:21:05 +0200

    :sparkles: Add logic to refresh tokens when the response fail due to this

--- app/Http/Integrations/ConsumingPassport/ConsumingPassportConnector.php
+++ app/Http/Integrations/ConsumingPassport/ConsumingPassportConnector.php
@@ -4,9 +4,12 @@ namespace App\Http\Integrations\ConsumingPassport;
 
 use App\Models\User;
 use Saloon\Contracts\Authenticator;
+use Saloon\Exceptions\Request\FatalRequestException;
+use Saloon\Exceptions\Request\RequestException;
 use Saloon\Helpers\OAuth2\OAuthConfig;
 use Saloon\Http\Auth\TokenAuthenticator;
 use Saloon\Http\Connector;
+use Saloon\Http\Request;
 use Saloon\Traits\OAuth2\AuthorizationCodeGrant;
 use Saloon\Traits\Plugins\AcceptsJson;
 
@@ -33,6 +36,26 @@ class ConsumingPassportConnector extends Connector
     protected function defaultAuth(): ?Authenticator
     {
         return new TokenAuthenticator( $this->user->getConsumingPassportOAuthProvider()->getAccessToken() );
+    }
+
+    public function handleRetry(FatalRequestException|RequestException $exception, Request $request): bool
+    {
+        // Handle only out-of-date token errors
+        if ( ! $exception instanceof RequestException ) return false;
+        if ( $exception->getResponse()->status() !== 401 ) return false;
+
+        $this->refresh_tokens();
+
+        return true;
+    }
+
+    /**
+     * Refresh the tokens for the connector and update the user
+     */
+    protected function refresh_tokens(): void {
+        $new_authenticator = $this->refreshAccessToken( $this->user->getConsumingPassportOAuthProvider()->getRefreshToken() );
+        $this->authenticate( $new_authenticator );
+        $this->user->storeConsumingPassportOAuthProvider( $new_authenticator );
     }
 
     /**
