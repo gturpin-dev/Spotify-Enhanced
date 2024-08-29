207b12896c95568ff365d863281bc675374a03e7
Guillaume <guillaume.turpin45@gmail.com>
2024-08-29 18:15:48 +0200

    :rocket: Defines basic artists resource

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Resources\ArtistsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::resource('artists', ArtistsController::class)
+    // ->middleware('auth:api')
+    ->only([
+        'index',
+        'show'
+    ]);
