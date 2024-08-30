43dde893d909721f9281967f28cb816277abbb22
Guillaume <guillaume.turpin45@gmail.com>
2024-08-30 10:30:20 +0200

    :sparkles: Add the json:api spec package and develop first Artists resource

---
+++ app/JsonApi/V1/Artists/ArtistSchema.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\JsonApi\V1\Artists;
+
+use App\Models\Artist;
+use LaravelJsonApi\Eloquent\Schema;
+use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Fields\Str;
+use LaravelJsonApi\Eloquent\Fields\DateTime;
+use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
+use LaravelJsonApi\Eloquent\Contracts\Paginator;
+use LaravelJsonApi\Eloquent\Filters\Where;
+use LaravelJsonApi\Eloquent\Pagination\PagePagination;
+
+class ArtistSchema extends Schema
+{
+    /**
+     * The model the schema corresponds to.
+     *
+     * @var string
+     */
+    public static string $model = Artist::class;
+
+    /**
+     * The default sort order for this resource.
+     *
+     * @var string|string[]|null
+     */
+    protected $defaultSort = 'name';
+
+    /**
+     * The default paging parameters to use if the client supplies none.
+     *
+     * @var array|null
+     */
+    protected ?array $defaultPagination = [
+        'number' => 1,
+        'size'   => 100
+    ];
+
+    /**
+     * Get the resource fields.
+     *
+     * @return array
+     */
+    public function fields(): array
+    {
+        return [
+            ID::make(),
+            Str::make('name')->sortable(),
+            Str::make('spotify_id')->sortable(),
+            DateTime::make('createdAt')->sortable()->readOnly(),
+            DateTime::make('updatedAt')->sortable()->readOnly(),
+        ];
+    }
+
+    /**
+     * Get the resource filters.
+     *
+     * @return array
+     */
+    public function filters(): array
+    {
+        return [
+            WhereIdIn::make($this),
+            Where::make('name'),
+            Where::make('spotify_id'),
+        ];
+    }
+
+    /**
+     * Get the resource paginator.
+     *
+     * @return Paginator|null
+     */
+    public function pagination(): ?Paginator
+    {
+        return PagePagination::make();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,25 @@
 use App\Http\Controllers\Resources\ArtistsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
+use LaravelJsonApi\Laravel\Routing\ResourceRegistrar;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
 Route::resource('artists', ArtistsController::class)
-    // ->middleware('auth:api')
+    ->middleware('auth:api')
     ->only([
         'index',
         'show'
     ]);
+
+JsonApiRoute::server('v1')
+    ->prefix('v1')
+    ->name('api:v1')
+    // ->middleware('auth:api')
+    ->resources(function ( ResourceRegistrar $server) {
+        $server->resource('artists', JsonApiController::class)->readOnly();
+    });
